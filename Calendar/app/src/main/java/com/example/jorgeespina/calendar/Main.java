d75b87679414863e46a53ace209ce71426e52844
JorgeEspina <canchemolinas@gmail.com>
2017-05-07 21:55:13 -0600

    Calendar Android

---
+++ Calendar/app/src/main/java/com/example/jorgeespina/calendar/Main.java
@@ -0,0 +1,33 @@
+package com.example.jorgeespina.calendar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton boton = (ImageButton) findViewById(R.id.Login);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                Intent nuevoform = new Intent(Main.this,Principal.class);
+                startActivity(nuevoform);
+            }
+        });
+
+        ImageButton boton1 = (ImageButton) findViewById(R.id.Registro);
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                Intent nuevoform = new Intent(Main.this,Registro.class);
+                startActivity(nuevoform);
+            }
+        });
+    }
+}
